38267db852f0d5fd14a58757e21fd53523b6af16
andy <andy.william.mcdonald@gmail.com>
2020-08-27 11:04:25 +0100

    Updates

---
+++ precache-manifest.affbc859bb9ed0aad8462f0501000d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41418c3ad5fa240fe3fc846a832bfc57",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "92edbd1e5925dfe985da",
+    "url": "/markdown-previewer/static/css/main.b4d1fa3c.chunk.css"
+  },
+  {
+    "revision": "b0b4f8e6d435093fd7d9",
+    "url": "/markdown-previewer/static/js/2.ccc42133.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.ccc42133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92edbd1e5925dfe985da",
+    "url": "/markdown-previewer/static/js/main.c4d9c139.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
